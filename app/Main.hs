cccac4533b7e505ca7c75a64b93f8830d3a9f5a0
Grant Maki <grantmaki@pm.me>
2021-08-03 10:53:23 -0400

    add sqllite db

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import Yesod
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Control.Monad.Trans.Resource (runResourceT)
+import Control.Monad.Logger (runStderrLoggingT)
 
 main :: IO ()
-main = warp 3000 FlashCards
+main = 
+  runStderrLoggingT $ withSqlitePool "test.db3" 5 $ \pool -> liftIO $ do
+    runResourceT $ flip runSqlPool pool $ do
+        runMigration migrateAll
+        card <- getBy $ UniqueQuestion "question1"
+        case card of
+          Nothing ->
+            do
+              cardId <- insert $ Card "question1" "answer1"
+              pure ()
+          Just card ->
+            pure ()
+    warp 3000 $ FlashCards pool
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,24 +4,66 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Lib where
 
 import Yesod
+import Database.Persist.Sqlite
+import Data.Text
 
-instance Yesod FlashCards where
-  shouldLogIO FlashCards _ _ = 
-    pure True
-
-data FlashCards = FlashCards
+share [mkPersist sqlSettings, mkMigrate "migrateAll"][persistLowerCase|
+Card
+  question Text
+  UniqueQuestion question
+  answer Text
+  deriving Show
+|]
 
 mkYesod "FlashCards" [parseRoutes|
-/flashcard/#Int FlashCardR GET
+/flashcard/#CardId FlashCardR GET POST
 |]
 
 
-getFlashCardR :: Int -> HandlerFor FlashCards Value
-getFlashCardR number = do
-  $logDebug "FlashCardR GET"
-  pure $ object ["msg" .= ("you got flashcard number " ++ (show number) :: String)]
+instance Yesod FlashCards where
+  shouldLogIO (FlashCards _) _ _ = 
+    pure True
 
+data FlashCards = FlashCards ConnectionPool
+
+getFlashCardR :: CardId -> HandlerFor FlashCards Value
+getFlashCardR cardId = do
+  $logDebug "FlashCardR GET"
+  card <- runDB $ get404 cardId
+  returnJson $ card
+
+postFlashCardR :: CardId -> HandlerFor FlashCards Value
+postFlashCardR cardId = do
+  $logDebug "FlashCardR POST"
+  $logDebug (pack $ show cardId)
+  runDB $ insert_ $ Card (pack $ "q" ++ show cardId) ( pack $ "a" ++ show cardId)
+  card <- runDB $ get404 cardId
+  returnJson $ card
+
+
+
+instance YesodPersist FlashCards where
+  type YesodPersistBackend FlashCards = SqlBackend
+
+  runDB action = do 
+    FlashCards pool <- getYesod
+    runSqlPool action pool
+
+instance ToJSON Card where
+  toJSON Card {..} = object
+    ["question" .= cardQuestion
+    , "answer" .= cardAnswer
+    ]
